be34e6802e9b8c3565479cb4e40e8e5d517106c0
Pierre Masselot <pmasselot2003@gmail.com>
2020-11-19 00:46:54 -0500

    added load and unload sequence + formatting

--- Marlin/Marlin/src/feature/mpmmu/mpmmu.cpp
+++ Marlin/Marlin/src/feature/mpmmu/mpmmu.cpp
@@ -1,7 +1,7 @@
 #include "../../inc/MarlinConfig.h"
 #if ENABLED(MMU_CLONE)
 
-#include"mpmmu.h"
+#include "mpmmu.h"
 #include "../../MarlinCore.h"
 #include "../../gcode/gcode.h"
 #include "../../module/planner.h"
@@ -10,82 +10,142 @@
 #include "../../gcode/parser.h"
 #include "../../module/endstops.h"
 
-boolean idlerHomed=false;//idler status
-boolean homingidler=false;//idler currently homing
+boolean idlerHomed = false;  //idler status
+boolean homingidler = false; //idler currently homing
 float idlerPosition;
-float offsetEndstopTo1=0.3;//space from the endstop to the first bearing position(Filament 1)
-float spaceBetweenBearings=0.65;//space in between each bearing
-float absolutePosition;
+float offsetEndstopTo1 = 0.3;      //space from the endstop to the first bearing position(Filament 1)
+float spaceBetweenBearings = 0.75; //space in between each bearing
+float absolutePosition;            //position for the idler to be pressing on the correct filament
+float MMUToNozzleLength = 40;      //length, for now the unit is arbitrary but will have to set correct step per mm to get it in mm or scale acordingly
 
-void MPMMU::tool_change(uint8_t index){
-  if(idlerHomed==false){
-    homingidler=true;
+void MPMMU::tool_change(uint8_t index)
+{
+  if (idlerHomed == false)
+  {
+    homingidler = true;
     endstops.enable(true);
-    
-    planner.buffer_line(0,0,0,current_position.e+1000,1,1);//move towards endstop until it's hit
-    planner.synchronize();//wait for the move to finish
+
+    planner.buffer_line(0, 0, 0, current_position.e + 1000, 1, 1); //move towards endstop until it's hit
+    planner.synchronize();                                         //wait for the move to finish
     endstops.validate_homing_move();
-    homingidler=false;//homing completed
-    idlerPosition=0;//new idler position
-    planner.position.resetExtruder();//reset the extruder position to 0 to avoid problems with next move
+    homingidler = false;              //homing completed
+    idlerPosition = 0;                //new idler position
+    planner.position.resetExtruder(); //reset the extruder position to 0 to avoid problems with next move
     endstops.not_homing();
-    
-    idlerHomed=true;
+
+    idlerHomed = true;
   }
 
-  switch(index){
-    case 0:
-       SERIAL_ECHOPGM("change to filament #1" );
-       SERIAL_ECHOLNPAIR("change to filament #1" );
-       absolutePosition=offsetEndstopTo1;
-       planner.buffer_line(0,0,0,-(absolutePosition-idlerPosition),2,1);
-       planner.synchronize();
-       planner.position.resetExtruder();
-       idlerPosition= absolutePosition;
-       
-       break;
-    case 1:
-       SERIAL_ECHOPGM("change to filament #2" );
-       SERIAL_ECHOLNPAIR("change to filament #2" );
-       absolutePosition=offsetEndstopTo1+1*spaceBetweenBearings;
-       planner.buffer_line(0,0,0,-(absolutePosition-idlerPosition),2,1);
-       planner.synchronize();
-       planner.position.resetExtruder();
-       idlerPosition= absolutePosition;
-       break;
-    case 2:
-      SERIAL_ECHOPGM("change to filament #3" );
-      SERIAL_ECHOLNPAIR("change to filament #3" );
-      absolutePosition=offsetEndstopTo1+2*spaceBetweenBearings;
-       planner.buffer_line(0,0,0,-(absolutePosition-idlerPosition),2,1);
-       planner.synchronize();
-       planner.position.resetExtruder();
-       idlerPosition= absolutePosition;
-      break;
-    case 3:
-      SERIAL_ECHOPGM("change to filament #4" );
-      SERIAL_ECHOLNPAIR("change to filament #4" );
-      absolutePosition=3*spaceBetweenBearings;
-       planner.buffer_line(0,0,0,-(absolutePosition-idlerPosition),2,1);
-       planner.synchronize();
-       planner.position.resetExtruder();
-       idlerPosition= absolutePosition;
-      break;
-    case 4:
-       SERIAL_ECHOPGM("change to filament #5" );
-       SERIAL_ECHOLNPAIR("change to filament #5" );
-       absolutePosition=offsetEndstopTo1+4*spaceBetweenBearings;
-       planner.buffer_line(0,0,0,-(absolutePosition-idlerPosition),2,1);
-       planner.synchronize();
-       planner.position.resetExtruder();
-       idlerPosition= absolutePosition;
-       break;
+  switch (index)
+  {
+  case 0:
+    SERIAL_ECHOPGM("change to filament #1");
+    SERIAL_ECHOLNPAIR("change to filament #1");
 
+    //unload filament
+    planner.buffer_line(0, 0, 0, -MMUToNozzleLength, 4, 0);
+    planner.synchronize();
+    planner.position.resetExtruder();
+    //idler select new filament
+    absolutePosition = offsetEndstopTo1;
+    planner.buffer_line(0, 0, 0, -(absolutePosition - idlerPosition), 2, 1);
+    planner.synchronize();
+    planner.position.resetExtruder();
+    //reload the new filament
+    planner.buffer_line(0, 0, 0, MMUToNozzleLength, 4, 0);
+    planner.synchronize();
+    planner.position.resetExtruder();
+    idlerPosition = absolutePosition;
+
+    break;
+  case 1:
+    SERIAL_ECHOPGM("change to filament #2");
+    SERIAL_ECHOLNPAIR("change to filament #2");
+
+    //unload filament
+    planner.buffer_line(0, 0, 0, -MMUToNozzleLength, 4, 0);
+    planner.synchronize();
+    planner.position.resetExtruder();
+
+    //idler select new filament
+    absolutePosition = offsetEndstopTo1 + 1 * spaceBetweenBearings;
+    planner.buffer_line(0, 0, 0, -(absolutePosition - idlerPosition), 2, 1);
+    planner.synchronize();
+    planner.position.resetExtruder();
+
+    //reload the new filament
+    planner.buffer_line(0, 0, 0, MMUToNozzleLength, 4, 0);
+    planner.synchronize();
+    planner.position.resetExtruder();
+    idlerPosition = absolutePosition;
+    break;
+  case 2:
+    SERIAL_ECHOPGM("change to filament #3");
+    SERIAL_ECHOLNPAIR("change to filament #3");
+
+    //unload filament
+    planner.buffer_line(0, 0, 0, -MMUToNozzleLength, 4, 0);
+    planner.synchronize();
+    planner.position.resetExtruder();
+
+    //idler select new filament
+    absolutePosition = offsetEndstopTo1 + 2 * spaceBetweenBearings;
+    planner.buffer_line(0, 0, 0, -(absolutePosition - idlerPosition), 2, 1);
+    planner.synchronize();
+    planner.position.resetExtruder();
+
+    //reload the new filament
+    planner.buffer_line(0, 0, 0, MMUToNozzleLength, 4, 0);
+    planner.synchronize();
+    planner.position.resetExtruder();
+    idlerPosition = absolutePosition;
+    break;
+  case 3:
+    SERIAL_ECHOPGM("change to filament #4");
+    SERIAL_ECHOLNPAIR("change to filament #4");
+
+    //unload filament
+    planner.buffer_line(0, 0, 0, -MMUToNozzleLength, 4, 0);
+    planner.synchronize();
+    planner.position.resetExtruder();
+
+    //idler select new filament
+    absolutePosition = offsetEndstopTo1 + 3 * spaceBetweenBearings;
+    planner.buffer_line(0, 0, 0, -(absolutePosition - idlerPosition), 2, 1);
+    planner.synchronize();
+    planner.position.resetExtruder();
+
+    //reload the new filament
+    planner.buffer_line(0, 0, 0, MMUToNozzleLength, 4, 0);
+    planner.synchronize();
+    planner.position.resetExtruder();
+    idlerPosition = absolutePosition;
+    break;
+  case 4:
+    SERIAL_ECHOPGM("change to filament #5");
+    SERIAL_ECHOLNPAIR("change to filament #5");
+
+    //unload filament
+    planner.buffer_line(0, 0, 0, -MMUToNozzleLength, 4, 0);
+    planner.synchronize();
+    planner.position.resetExtruder();
+
+    //idler select new filament
+    absolutePosition = offsetEndstopTo1 + 4 * spaceBetweenBearings;
+    planner.buffer_line(0, 0, 0, -(absolutePosition - idlerPosition), 2, 1);
+    planner.synchronize();
+    planner.position.resetExtruder();
+
+    //reload the new filament
+    planner.buffer_line(0, 0, 0, MMUToNozzleLength, 4, 0);
+    planner.synchronize();
+    planner.position.resetExtruder();
+    idlerPosition = absolutePosition;
+    break;
   }
-  
- 
 }
-boolean MPMMU::idler_is_moving(){
+boolean MPMMU::idler_is_moving()
+{
   return homingidler;
 }
 
