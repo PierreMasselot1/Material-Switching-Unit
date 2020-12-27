ed906477881ee5d90312bff86993fc04ca2b4dc1
Pierre Masselot <pmasselot2003@gmail.com>
2020-12-27 00:13:04 -0500

    config changes and single nozzle enabled

--- Marlin/Marlin/Configuration.h
+++ Marlin/Marlin/Configuration.h
@@ -147,7 +147,7 @@
 #define DEFAULT_NOMINAL_FILAMENT_DIA 1.75
 
 // For Cyclops or any "multi-extruder" that shares a single nozzle.
-//#define SINGLENOZZLE
+#define SINGLENOZZLE
 
 // Save and restore temperature and fan speed on tool-change.
 // Set standby for the unselected tool with M104/106/109 T...
