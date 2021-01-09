c4fff34bbbd863b535b24567a585e93670f25c29
Pierre Masselot <pmasselot2003@gmail.com>
2021-01-08 21:39:52 -0500

    small fixes

--- Marlin/Marlin/src/feature/mpmmu/mpmmu.cpp
+++ Marlin/Marlin/src/feature/mpmmu/mpmmu.cpp
@@ -13,17 +13,25 @@
 bool idlerHomed = false;  //idler status
 bool homingidler = false; //idler currently homing
 float idlerPosition;
+
+float offsetEndstopTo1 = 0.3 * 4.16;          //space from the endstop to the first bearing position(Filament 1)
+
+float spaceBetweenBearings = 0.75 * 4.16;     //space in between each bearing
 float parkedPosition = 0;
-float offsetEndstopTo1 = 1.250;                 //space from the endstop to the first bearing position(Filament 1)
-float spaceBetweenBearings =3.12;            //space in between each bearing
 float absolutePosition;                       //position for the idler to be pressing on the correct filament
 float MMUToNozzleLength = BOWDEN_TUBE_LENGTH; //length, for now the unit is arbitrary but will have to set correct step per mm to get it in mm or scale acordingly
 float storeExtruderPosition;
 void MPMMU::tool_change(uint8_t index)
-{ storeExtruderPosition=planner.position.e;
+{
+  storeExtruderPosition = planner.position.e;
   if (idlerHomed == false)
   {
     idler_home();
+    //get the idler out of the way after homing
+    planner.buffer_line(0, 0, 0, -(parkedPosition - idlerPosition), 2, MMU_IDLER_PIN);
+    planner.synchronize();
+    planner.position.resetExtruder();
+    idlerPosition = parkedPosition;
   }
   planner.position.resetExtruder(); //reset the extruder position to 0 to avoid problems with next move
   //unload filament slow
@@ -31,9 +39,9 @@ void MPMMU::tool_change(uint8_t index)
 #ifdef DIRECT_DRIVE
   planner.buffer_line(0, 0, 0, -10, 4, EXTRUDER_PIN);
 #endif
-planner.buffer_line(planner.get_axis_position_mm(X_AXIS), planner.get_axis_position_mm(Y_AXIS), planner.get_axis_position_mm(Z_AXIS), -MMUToNozzleLength, 16, MMU_EXTRUDER_PIN);
+  planner.buffer_line(planner.get_axis_position_mm(X_AXIS), planner.get_axis_position_mm(Y_AXIS), planner.get_axis_position_mm(Z_AXIS), -MMUToNozzleLength, 16, MMU_EXTRUDER_PIN);
 #ifdef DIRECT_DRIVE
-  planner.buffer_line(planner.get_axis_position_mm(X_AXIS), planner.get_axis_position_mm(Y_AXIS), planner.get_axis_position_mm(Z_AXIS),, -MMUToNozzleLength+10, 16, EXTRUDER_PIN);
+  planner.buffer_line(planner.get_axis_position_mm(X_AXIS), planner.get_axis_position_mm(Y_AXIS), planner.get_axis_position_mm(Z_AXIS), , -MMUToNozzleLength + 10, 16, EXTRUDER_PIN);
 #endif
   planner.synchronize();
   planner.position.resetExtruder();
@@ -49,7 +57,7 @@ planner.buffer_line(planner.get_axis_position_mm(X_AXIS), planner.get_axis_posit
 #ifdef DIRECT_DRIVE
   planner.buffer_line(planner.get_axis_position_mm(X_AXIS), planner.get_axis_position_mm(Y_AXIS), planner.get_axis_position_mm(Z_AXIS), 10, 4, EXTRUDER_PIN);
 #endif
-//reload the new filament fast
+  //reload the new filament fast
   planner.buffer_line(planner.get_axis_position_mm(X_AXIS), planner.get_axis_position_mm(Y_AXIS), planner.get_axis_position_mm(Z_AXIS), MMUToNozzleLength, 16, MMU_EXTRUDER_PIN);
 #ifdef DIRECT_DRIVE
   planner.buffer_line(planner.get_axis_position_mm(X_AXIS), planner.get_axis_position_mm(Y_AXIS), planner.get_axis_position_mm(Z_AXIS), MMUToNozzleLength, 16, EXTRUDER_PIN);
@@ -57,7 +65,7 @@ planner.buffer_line(planner.get_axis_position_mm(X_AXIS), planner.get_axis_posit
   planner.synchronize();
   planner.position.resetExtruder();
   idlerPosition = absolutePosition;
-  planner.position.e=storeExtruderPosition;
+  planner.position.e = storeExtruderPosition;
 //if direct drive is enabled park the idler leting the filament move freely through the MMU
 #ifdef DIRECT_DRIVE
   absolutePosition = parkedPosition;
@@ -72,7 +80,7 @@ void MPMMU::idler_home()
   endstops.enable(true);
 
   planner.buffer_line(0, 0, 0, current_position.e + 100, 1, MMU_IDLER_PIN); //move towards endstop until it's hit
-  planner.synchronize();                                                     //wait for the move to finish
+  planner.synchronize();                                                    //wait for the move to finish
   endstops.validate_homing_move();
   homingidler = false;              //homing completed
   idlerPosition = 0;                //new idler position
