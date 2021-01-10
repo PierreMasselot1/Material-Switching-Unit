b746e076865793fc307fe688afe14a8030bd443a
Pierre Masselot <pmasselot2003@gmail.com>
2021-01-10 12:13:27 -0500

    motion limits bugfix

--- Marlin/Marlin/src/feature/mpmmu/mpmmu.cpp
+++ Marlin/Marlin/src/feature/mpmmu/mpmmu.cpp
@@ -20,37 +20,40 @@ float parkedPosition = 0;
 float absolutePosition;                       //position for the idler to be pressing on the correct filament
 float MMUToNozzleLength = BOWDEN_TUBE_LENGTH; //length, for now the unit is arbitrary but will have to set correct step per mm to get it in mm or scale acordingly
 float storeExtruderPosition;
+xyz_pos_t position= xyz_pos_t(current_position);
+
 void MPMMU::tool_change(uint8_t index)
-{
+{ position= xyz_pos_t(current_position);
+  apply_motion_limits(position);
   storeExtruderPosition = planner.position.e;
   planner.position.resetExtruder(); //reset the extruder position to 0 to avoid problems with next move
   //unload filament slow
-  planner.buffer_line(planner.get_axis_position_mm(X_AXIS), planner.get_axis_position_mm(Y_AXIS), planner.get_axis_position_mm(Z_AXIS), -10, 4, MMU_EXTRUDER_PIN);
+  planner.buffer_line(position, -10, 4, MMU_EXTRUDER_PIN);
 #ifdef DIRECT_DRIVE
   planner.buffer_line(0, 0, 0, -10, 4, EXTRUDER_PIN);
 #endif
-  planner.buffer_line(planner.get_axis_position_mm(X_AXIS), planner.get_axis_position_mm(Y_AXIS), planner.get_axis_position_mm(Z_AXIS), -MMUToNozzleLength, 16, MMU_EXTRUDER_PIN);
+  planner.buffer_line(position, -MMUToNozzleLength, 16, MMU_EXTRUDER_PIN);
 #ifdef DIRECT_DRIVE
-  planner.buffer_line(planner.get_axis_position_mm(X_AXIS), planner.get_axis_position_mm(Y_AXIS), planner.get_axis_position_mm(Z_AXIS), , -MMUToNozzleLength + 10, 16, EXTRUDER_PIN);
+  planner.buffer_line(position , -MMUToNozzleLength + 10, 16, EXTRUDER_PIN);
 #endif
   planner.synchronize();
   planner.position.resetExtruder();
 
   //idler select new filament
   absolutePosition = offsetEndstopTo1 + index * spaceBetweenBearings;
-  planner.buffer_line(planner.get_axis_position_mm(X_AXIS), planner.get_axis_position_mm(Y_AXIS), planner.get_axis_position_mm(Z_AXIS), -(absolutePosition - idlerPosition), 2, MMU_IDLER_PIN);
+  planner.buffer_line(position, -(absolutePosition - idlerPosition), 2, MMU_IDLER_PIN);
   planner.synchronize();
   planner.position.resetExtruder();
 
   //reload the new filament slow
-  planner.buffer_line(planner.get_axis_position_mm(X_AXIS), planner.get_axis_position_mm(Y_AXIS), planner.get_axis_position_mm(Z_AXIS), 10, 4, MMU_EXTRUDER_PIN);
+  planner.buffer_line(position, 10, 4, MMU_EXTRUDER_PIN);
 #ifdef DIRECT_DRIVE
-  planner.buffer_line(planner.get_axis_position_mm(X_AXIS), planner.get_axis_position_mm(Y_AXIS), planner.get_axis_position_mm(Z_AXIS), 10, 4, EXTRUDER_PIN);
+  planner.buffer_line(position, 10, 4, EXTRUDER_PIN);
 #endif
   //reload the new filament fast
-  planner.buffer_line(planner.get_axis_position_mm(X_AXIS), planner.get_axis_position_mm(Y_AXIS), planner.get_axis_position_mm(Z_AXIS), MMUToNozzleLength, 16, MMU_EXTRUDER_PIN);
+  planner.buffer_line(position, MMUToNozzleLength, 16, MMU_EXTRUDER_PIN);
 #ifdef DIRECT_DRIVE
-  planner.buffer_line(planner.get_axis_position_mm(X_AXIS), planner.get_axis_position_mm(Y_AXIS), planner.get_axis_position_mm(Z_AXIS), MMUToNozzleLength, 16, EXTRUDER_PIN);
+  planner.buffer_line(position, MMUToNozzleLength, 16, EXTRUDER_PIN);
 #endif
   planner.synchronize();
   planner.position.resetExtruder();
@@ -68,8 +71,10 @@ void MPMMU::idler_home()
 {
   homingIdler = true;
   endstops.enable(true);
+  position= xyz_pos_t(current_position);
+  apply_motion_limits(position);
 
-  planner.buffer_line(planner.get_axis_position_mm(X_AXIS),  planner.get_axis_position_mm(Y_AXIS), planner.get_axis_position_mm(Z_AXIS), current_position.e + 100, 1, MMU_IDLER_PIN); //move towards endstop until it's hit
+  planner.buffer_line(position, current_position.e + 100, 1, MMU_IDLER_PIN); //move towards endstop until it's hit
   planner.synchronize();                                                    //wait for the move to finish
   endstops.validate_homing_move();
   homingIdler = false;              //homing completed
