aa3b7aeb3512a040ab80843f3c8299d3a7ebe7d0
Pierre Masselot <pmasselot2003@gmail.com>
2021-01-21 15:46:12 -0500

    added menu message

--- Marlin/Marlin/src/lcd/language/language_en.h
+++ Marlin/Marlin/src/lcd/language/language_en.h
@@ -544,6 +544,8 @@ namespace Language_en {
   PROGMEM Language_Str MSG_KILL_HOMING_FAILED              = _UxGT("Homing Failed");
   PROGMEM Language_Str MSG_LCD_PROBING_FAILED              = _UxGT("Probing Failed");
 
+  PROGMEM Language_Str MSG_MSU_MENU                       = _UxGT("MSU");
+
   PROGMEM Language_Str MSG_MMU2_CHOOSE_FILAMENT_HEADER     = _UxGT("CHOOSE FILAMENT");
   PROGMEM Language_Str MSG_MMU2_MENU                       = _UxGT("MMU");
   PROGMEM Language_Str MSG_KILL_MMU2_FIRMWARE              = _UxGT("Update MMU Firmware!");
