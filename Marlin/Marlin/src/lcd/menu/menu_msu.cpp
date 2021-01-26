c193ea02415d84fa8cc21e7a0018dbbf82a8efaf
Pierre Masselot <pmasselot2003@gmail.com>
2021-01-25 19:21:28 -0500

    bowden calibration ground work

--- Marlin/Marlin/src/lcd/menu/menu_msu.cpp
+++ Marlin/Marlin/src/lcd/menu/menu_msu.cpp
@@ -44,5 +44,36 @@ void menu_msu_change_filament() {
   END_MENU();
 }
 
+void menu_msu_adjust_bowden_length(){
+  START_MENU();
+  BACK_ITEM(MSG_MSU_MENU);
+}
+
+ static void lcd_move_e(TERN_(MULTI_MANUAL, const int8_t eindex=-1)) {
+    if (ui.use_click()) return ui.goto_previous_screen_no_defer();
+    if (ui.encoderPosition) {
+      if (!ui.manual_move.processing) {
+        const float diff = float(int32_t(ui.encoderPosition)) * ui.manual_move.menu_scale;
+        TERN(IS_KINEMATIC, ui.manual_move.offset, current_position.e) += diff;
+        ui.manual_move.soon(E_AXIS
+          #if MULTI_MANUAL
+            , eindex
+          #endif
+        );
+        ui.refresh(LCDVIEW_REDRAW_NOW);
+      }
+      ui.encoderPosition = 0;
+    }
+    if (ui.should_draw()) {
+      TERN_(MULTI_MANUAL, MenuItemBase::init(eindex));
+      MenuEditItemBase::draw_edit_screen(
+        GET_TEXT(TERN(MULTI_MANUAL, MSG_MOVE_EN, MSG_MOVE_E)),
+        ftostr41sign(current_position.e
+          + TERN0(IS_KINEMATIC, ui.manual_move.offset)
+          - TERN0(MANUAL_E_MOVES_RELATIVE, manual_move_e_origin)
+        )
+      );
+    } // should_draw
+ }
 
 #endif // HAS_LCD_MENU && MMU2_MENUS
