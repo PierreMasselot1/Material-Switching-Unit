93cd0d5dc74d1ac5e9b3a1b17c665426d6011bb6
Pierre Masselot <pmasselot2003@gmail.com>
2021-03-30 23:13:54 -0400

    Disable endstop settings when using a servo motor

--- Marlin/Marlin/Configuration.h
+++ Marlin/Marlin/Configuration.h
@@ -218,10 +218,12 @@
   #if ENABLED(DIRECT_DRIVE)
     #define ORIGINAL_EXTRUDER_ENBR 2//define the extruder nbr that the actual extruder is connected to 
   #endif
- 
-  #define IDLER_ENDSTOP_AXIS X //select the endstop connected
-  #define IDLER_ENDSTOP_MINMAX MAX
-  //#defin IDLER_ENDSTOP_PIN  //TODO              for a custom endstop pin. 
+
+  #if ENABLED(SERVO_IDLER)
+    #define IDLER_ENDSTOP_AXIS X //select the endstop connected
+    #define IDLER_ENDSTOP_MINMAX MAX
+    //#defin IDLER_ENDSTOP_PIN  //TODO              for a custom endstop pin. 
+  #endif
 
   #define BOWDEN_TUBE_LENGTH 200//bowden tube length from the crossing point of the merger to the nozzle
   #define NOZZLE_EXTRUDER_GEAR_LENGTH 40 //Only necessary for direct drive setups, the distance from the extruder gear up to the nozzle. 
